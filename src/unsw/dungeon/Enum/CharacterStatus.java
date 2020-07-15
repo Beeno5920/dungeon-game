77b95b10f59d950172040a3029ee8bf80ef3b2d1
Chi Chung Cheung <z5268837@unsw.edu.au>
2020-07-15 23:24:51 +1000

    feat: add enums for orientation, item category and character status

---
+++ src/unsw/dungeon/Enum/CharacterStatus.java
@@ -0,0 +1,7 @@
+package unsw.dungeon.Enum;
+
+public enum CharacterStatus {
+    NORMAL,
+    DEAD,
+    INVINCIBLE
+}
---
+++ src/unsw/dungeon/Enum/ItemCategory.java
@@ -0,0 +1,7 @@
+package unsw.dungeon.Enum;
+
+public enum ItemCategory {
+    KEY,
+    SWORD,
+    POTION
+}
