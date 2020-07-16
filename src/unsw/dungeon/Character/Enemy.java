d731810913267c480649972e8293bdf0d13739dc
Chi Chung Cheung <z5268837@unsw.edu.au>
2020-07-17 00:17:08 +1000

    feat: Enemy is now observable

--- src/unsw/dungeon/Character/Enemy.java
+++ src/unsw/dungeon/Character/Enemy.java
@@ -2,14 +2,24 @@ package unsw.dungeon.Character;
 
 import unsw.dungeon.Dungeon;
 import unsw.dungeon.Entity;
+import unsw.dungeon.Observable;
 import unsw.dungeon.Observer;
 
-public class Enemy extends Character implements Observer {
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Enemy extends Character implements Observer, Observable {
     int speed;
+    int[] playerPosition;
+    Set<Observer> observers;
 
     public Enemy(Dungeon dungeon, int x, int y) {
         super(dungeon, x, y);
         this.speed = 5;
+        this.playerPosition = dungeon.getPlayer().getPosition();
+        this.observers = new HashSet<>();
     }
 
     public void trackPlayer() {
@@ -18,6 +28,44 @@ public class Enemy extends Character implements Observer {
 
     @Override
     public void update(Entity entity) {
-        //TODO
+        if (entity instanceof Player)
+            playerPosition = ((Player) entity).getPosition();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Enemy enemy = (Enemy) o;
+        return speed == enemy.speed &&
+                Arrays.equals(playerPosition, enemy.playerPosition);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(speed);
+        result = 31 * result + Arrays.hashCode(playerPosition);
+        return result;
+    }
+
+    @Override
+    public void notify(Observer object) {
+        object.update(this);
+    }
+
+    @Override
+    public void notifyAllObservers() {
+        for (Observer observer : observers)
+            notify(observer);
+    }
+
+    @Override
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
     }
 }
