c20b88577cee1d57d8e4fded6cb763c0482c0402
Chi Chung Cheung <z5268837@unsw.edu.au>
2020-07-18 22:16:08 +1000

    feat: turn the player to invincible for 5 seconds after pick up the potion

--- src/unsw/dungeon/Item/InvincibilityPotion.java
+++ src/unsw/dungeon/Item/InvincibilityPotion.java
@@ -1,16 +1,29 @@
 package unsw.dungeon.Item;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
+import unsw.dungeon.Character.Character;
 import unsw.dungeon.Character.Player;
 import unsw.dungeon.Enum.CharacterStatus;
 import unsw.dungeon.Enum.ItemCategory;
 
 public class InvincibilityPotion extends Item {
+    private Player player;
+    private Timeline timeline;
+
     public InvincibilityPotion(ItemCategory itemCategory, int x, int y) {
         super(itemCategory, x, y);
+        this.timeline = new Timeline();
+
+        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(5), e -> player.setCharacterStatus(CharacterStatus.NORMAL)));
     }
 
     @Override
     public void onPick(Player player) {
+        this.player = player;
         player.setCharacterStatus(CharacterStatus.INVINCIBLE);
+        player.notifyAllObservers();
+        timeline.playFromStart();
     }
 }
