14426b47119bfea509a7b0dabd540800815bb9a7
Chi Chung Cheung <z5268837@unsw.edu.au>
2020-07-20 00:44:56 +1000

    refactor: move to Test directory

--- src/Test/MattTest.java
+++ src/Test/MattTest.java
@@ -1,10 +1,8 @@
-package unsw.dungeon.Test;
+package Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import unsw.dungeon.Dungeon;
 
