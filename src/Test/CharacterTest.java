d6d67d499284c68c21681741d3d4d0dc3419e6f2
Chi Chung Cheung <z5268837@unsw.edu.au>
2020-07-20 00:39:35 +1000

    refactor: remove the constructor and extends it to EntityTest class

--- src/Test/CharacterTest.java
+++ src/Test/CharacterTest.java
@@ -1,37 +1,12 @@
-package unsw.dungeon.Test;
+package Test;
 
-import unsw.dungeon.Character.Player;
-import unsw.dungeon.Dungeon;
 import unsw.dungeon.Enum.Orientation;
-import unsw.dungeon.FieldObject.Wall;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-public class CharacterTest {
-    protected int WIDTH = 50;
-    protected int HEIGHT = 50;
-
-    protected Dungeon dungeon;
-    protected Player player;
-
-    public CharacterTest() {
-        this.dungeon = new Dungeon(WIDTH, HEIGHT);
-        this.player = new Player(this.dungeon, 1, 1);
-        this.dungeon.setPlayer(this.player);
-        this.dungeon.addEntity(1, 1, player);
-
-        for (int i = 0; i < WIDTH; i++) {
-            dungeon.addEntity(i, 0, new Wall(i, 0));
-            dungeon.addEntity(i, HEIGHT - 1, new Wall(i, HEIGHT - 1));
-        }
-
-        for (int i = 1; i < HEIGHT - 1; i++) {
-            dungeon.addEntity(0, i, new Wall(0, i));
-            dungeon.addEntity(WIDTH - 1, i, new Wall(WIDTH - 1, i));
-        }
-    }
+public class CharacterTest extends EntityTest {
 
     @Test
     void testBasicMovement() {
--- src/Test/MattTest.java
+++ src/Test/MattTest.java
@@ -1,4 +1,4 @@
-package test;
+package unsw.dungeon.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
